8be83f808bd8e6a925dcda5350c1033676fc2322
ctsai12 <ctsai12@illinois.edu>
2018-03-19 08:36:42 -0500

    Only keep CoNLL experiments

--- Utils.h
+++ Utils.h
@@ -13,8 +13,8 @@
 #include "FeatureExtractor.h"
 #include <algorithm>
 //#include <linear-poly2.h>
-#include <linear-weight.h>
-//#include <linear.h>
+//#include <linear-weight.h>
+#include <linear.h>
 //#include "liblinear-poly2-2.01/linear.h"
 
 extern unordered_map<string, string> *label2id;
@@ -31,24 +31,14 @@ void predict_ner(vector<Document *> *docs, FeatureExtractor *extractor, model *m
 double evaluate_phrases(vector<Document *> *docs);
 void select_features(struct problem *train_prob, FeatureExtractor *extractor, double c, string out_file);
 void free_docs(vector<Document *> *docs);
-void writeTACFormat(vector<Document *> *docs, string outfile, string noun_type);
 void free_models(struct model **models);
 void writeColumnFormatFiles(vector<Document *> *docs, string dir);
 
 // experimental codes
 void select_features1(struct problem *train_prob, FeatureExtractor *extractor, double c, string out_file);
-model ** train_ner1(struct problem **probs, int solver, double c, double eps, double gamma, double coef);
-void predict_ner1(vector<Document *> *docs, FeatureExtractor *extractor, model **model);
-struct problem ** build_svm_problem1(const char *train_dir, FeatureExtractor *extractor);
 
-struct problem * build_selftrain_problem1(vector<Document *> *docs, vector<Document *> *odocs, FeatureExtractor *extractor, model *model);
-struct problem * build_selftrain_problem(vector<Document *> *docs, vector<Document *> *prev_docs, FeatureExtractor *extractor, model *model);
-struct problem * build_semi_problem(vector<Document *> *docs, FeatureExtractor *extractor, model *model);
 struct problem * build_weighted_svm_problem(vector<Document *> *docs, FeatureExtractor *extractor, double ow);
-void copy_pred_labels(vector<Document *> *docs);
 void clear_features(vector<Document *> *docs);
 void print_label_stats(vector<Document *> *docs);
-int merge_pred_labels(vector<Document *> *docs);
-int merge_pred_labels1(vector<Document *> *docs);
 
 #endif //NER_UTILS_H
